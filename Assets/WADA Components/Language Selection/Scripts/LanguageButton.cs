647eeede10ac9356cbfaf9b556cbaa517ec5da7b
frenkie <frank@frankbosma.nl>
2026-09-23 13:38:50 +0200

    Volume update and button tweaking

--- Assets/WADA Components/Language Selection/Scripts/LanguageButton.cs
+++ Assets/WADA Components/Language Selection/Scripts/LanguageButton.cs
@@ -6,33 +6,52 @@ namespace Wada
 {
     public class LanguageButton : MonoBehaviour
     {
+        public GameObject Container;
         public Languages Language;
         public SoundEffect Sound;
         public SoundEffect ChooseLanguageVoice;
-        
+
+        bool languageChosen;
         bool triggered;
 
-
+        void PushIt()
+        {
+            iTween.MoveBy(Container,  iTween.Hash("z", .2f, "easeType", "easeOutCubic", "islocal", true, "duration", .2f) );
+            iTween.MoveBy(Container,  iTween.Hash("z", -.2f, "easeType", "easeOutCubic", "islocal", true, "duration", .2f ,"delay", .3f) );
+        }
+        
         void Start()
         {
+            GameEngine.OnLanguageSwitch += OnLanguageSelected;
             if ( ! ChooseLanguageVoice.gameObject.activeSelf )
             {   
                 ChooseLanguageVoice.gameObject.SetActive(true);
             }
         }
 
+        void OnDisable()
+        {
+            GameEngine.OnLanguageSwitch -= OnLanguageSelected;
+        }
+
+        void OnLanguageSelected(Languages newLanguage)
+        {
+            languageChosen = true;
+        }
+
         [Button]
         public void Trigger()
         {
-            if ( !triggered )
+            if ( !triggered && !languageChosen )
             {
                 triggered = true;
-                //PlayMusic();
+                PushIt();
+                PlayMusic();
                 
                 ChooseLanguageVoice.FadeOutAndKill();
                 
                 GameEngine.GetInstance().SwitchLanguage( Language );
-                GameEngine.GetInstance().ResumeTimeline();
+                GameEngine.GetInstance().Invoke("ResumeTimeline", 2);
 
                 // 12-07-2025
                 //BackgroundMusicManager.GetInstance().FadeIn( BackgroundMusicType.IntroB );
@@ -49,7 +68,7 @@ namespace Wada
 
         public void PlayMusic()
         {
-            //Sound.Play();
+            Sound.Play();
         }
     }
     
